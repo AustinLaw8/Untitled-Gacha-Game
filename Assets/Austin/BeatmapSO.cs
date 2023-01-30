678262c2e1c9732b6406960a2e580af0775b7ac1
Austin Law <austinlaw8@gmail.com>
2023-01-29 17:16:54 -0800

    Implements scene change

---
+++ Assets/Austin/BeatmapSO.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BeatmapSO : ScriptableObject
+{
+    public float bpm;
+    public float duration;
+    public AudioSource song;
+    // public AssetReference beatmap;
+}
