9fc4c07741a38f9947d0daf2bc075c19943cd276
pinlunix <pheonlu@gmail.com>
2023-05-21 16:15:25 -0700

    Text Update
    Header and description text gets updated based on cardID

--- Assets/CardSystem/CardInventory/CardPanel.cs
+++ Assets/CardSystem/CardInventory/CardPanel.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class TestPanel : MonoBehaviour
+public class CardPanel : MonoBehaviour
 {
     public void OpenProfile()
     {
--- Assets/CardSystem/CardInventory/ModalManager.cs
+++ Assets/CardSystem/CardInventory/ModalManager.cs
@@ -11,6 +11,8 @@ public class ModalManager : MonoBehaviour
     [SerializeField] CardManager cardManager;
     [SerializeField] RawImage cardImg;
     [SerializeField] RawImage enlargeImage;
+    public TextMeshProUGUI cardHeader;
+    public TextMeshProUGUI cardDesc;
     private CardSO card;
 
     //public GameObject body;
@@ -33,6 +35,8 @@ public class ModalManager : MonoBehaviour
     {
         card = cardManager.cardDB[cardID];
         cardImg.texture = card.cardArt;
+        cardHeader.text = string.Concat(card.zodiac.ToString(), ", ", card.title);
+        cardDesc.text = string.Concat("Title: ", card.title, "\n", "Artist: ", card.artist, "\n", "Zodiac: ", card.zodiac.ToString(), "\n", "Rarity: ", card.rarity.ToString(), "\n");
 
         modalWindow.SetActive(true);
     }
